452d7726f3c20ecc63037a3d4b18aef08748c104
antonio <asergiocosta.as@gmail.com>
2022-12-30 16:31:27 -0300

    Improve the user interface style

--- src/application/Interface.java
+++ src/application/Interface.java
@@ -4,25 +4,33 @@ import chessGame.entities.ChessPiece;
 
 public class Interface {
 		public static void printBoard(ChessPiece[][] ChessPieces) {
-			System.out.println("    _______________ ");
+			System.out.println("    _______________________________");			
 			for(int i = 0; i<ChessPieces.length; i++) {
-				System.out.print((8-i)+"| |");
+				System.out.println(" | |   |   |   |   |   |   |   |   |");
+				System.out.print((8-i)+"| ");
 				for(int j = 0; j< ChessPieces.length; j++) {
-				printPiece(ChessPieces[i][j]);	
+				printPiece(ChessPieces[i][j]);
 				}
-				System.out.println("");
+				System.out.println("|");
 			}
-			System.out.println("    _______________ ");
-			System.out.println("    a b c d e f g h ");
+			System.out.println("   =================================");
+			System.out.println("     a   b   c   d   e   f   g   h  ");
 		}
 
 		private static void printPiece(ChessPiece chessPiece) {
+
 			if (chessPiece == null) {
-				System.out.print("_");
+				System.out.print("|_");
+				System.out.print("-");
+
+				
+				
 			} else {
+				System.out.println("|_");
 				System.out.print(chessPiece);
 			}
-			System.out.print("|");
+			System.out.print("_");
+			
 		}
 		
 }

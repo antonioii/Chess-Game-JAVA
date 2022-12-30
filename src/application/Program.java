893bce02d780561053dc5d5bd9a1d2f414b85afb
antonio <asergiocosta.as@gmail.com>
2022-12-30 11:21:02 -0300

    Setting initial files to github

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+//ALG - CHESS GAME APP
+// 30/12/2022
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Setting initital commit");
+		
+		sc.close();
+	}
+
+}

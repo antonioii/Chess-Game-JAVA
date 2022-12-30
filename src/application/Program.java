86c7af85ca19da77aad3ffc809aa8c879c2e26b4
antonio <asergiocosta.as@gmail.com>
2022-12-30 11:32:47 -0300

    Add Position Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,13 +4,17 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardGame.entities.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Setting initital commit");
+		System.out.println("Testing position obj: ");
+		Position newPosition = new Position(sc.nextInt(), sc.nextInt());
+		System.out.println(newPosition);
 		
 		sc.close();
 	}

3036e89978b566a2c7ce5efbd9a376d1ddab9dd5
antonio <asergiocosta.as@gmail.com>
2022-12-31 14:24:20 -0300

    Add colors and change initial setup
    1. Add colors to the terminal pieces;
    2. Change the initial setup adding more pieces;
    3. Commenting the initialSetup method tests.

--- src/chessGame/entities/ChessMatch.java
+++ src/chessGame/entities/ChessMatch.java
@@ -1,9 +1,6 @@
 package chessGame.entities;
 
-import java.util.Random;
-
 import boardGame.entities.Board;
-import boardGame.entities.Position;
 import chessGame.entities.pieces.King;
 import chessGame.entities.pieces.Rook;
 
@@ -31,12 +28,29 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
+		/*TESTS SNIPPET:
 		//Testing snippets with random rows and columns:
 		board.placePiece(new King(board, Color.WHITE), new Position(new Random().nextInt(8),new Random().nextInt(8)));
 		board.placePiece(new King(board, Color.BLACK), new Position(new Random().nextInt(8),new Random().nextInt(8)));
 		board.placePiece(new Rook(board, Color.WHITE), new Position(new Random().nextInt(8),new Random().nextInt(8)));
+		
 		//Testing snippet with a given chess position:
 		placePieceInChessPosition('h',8, new Rook(board, Color.BLACK));
+		*/
+		
+		placePieceInChessPosition('c', 1, new Rook(board, Color.WHITE));
+		placePieceInChessPosition('c', 2, new Rook(board, Color.WHITE));
+		placePieceInChessPosition('d', 2, new Rook(board, Color.WHITE));
+		placePieceInChessPosition('e', 2, new Rook(board, Color.WHITE));
+		placePieceInChessPosition('e', 1, new Rook(board, Color.WHITE));
+		placePieceInChessPosition('d', 1, new King(board, Color.WHITE));
+
+		placePieceInChessPosition('c', 7, new Rook(board, Color.BLACK));
+		placePieceInChessPosition('c', 8, new Rook(board, Color.BLACK));
+		placePieceInChessPosition('d', 7, new Rook(board, Color.BLACK));
+		placePieceInChessPosition('e', 7, new Rook(board, Color.BLACK));
+		placePieceInChessPosition('e', 8, new Rook(board, Color.BLACK));
+		placePieceInChessPosition('d', 8, new King(board, Color.BLACK));
 		
 	}
 }

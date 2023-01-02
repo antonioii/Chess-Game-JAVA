5eed59d92284b7d31a76e1e93ee4b0c778424e57
antonio <asergiocosta.as@gmail.com>
2023-01-02 19:17:02 -0300

    Implement a loop to test game moviments
    1. Delete other tests from Program class;
    2. Implement a loop to test the game moviments;

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,8 @@
 //ALG - CHESS GAME APP
 // 30/12/2022
 package application;
-import java.util.InputMismatchException;
-import java.util.Random;
 import java.util.Scanner;
 
-import boardGame.entities.Board;
-import boardGame.entities.Position;
-import chessGame.entities.ChessException;
 import chessGame.entities.ChessMatch;
 import chessGame.entities.ChessPiece;
 import chessGame.entities.ChessPosition;
@@ -15,38 +10,25 @@ import chessGame.entities.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		//random number between 1 - 8:
-		int rand = new Random().nextInt(8)+1;
-		//Testing object Position:
-		System.out.println("Testing position obj: ");
-		Position newPosition = new Position(rand, rand);
-		System.out.println(newPosition);
-		System.out.println("");
-		
-		//Testing board creation:
-		Board board = new Board(rand, rand);
-		System.out.println("Testing board creation: ");
-		System.out.println(board.getRows()+", "+board.getColumns()+"\n");
-		
-		//Testing chess matches with random kings and castles:
-		System.out.println("Testing placePiece and printBoard with possible random exceptions:");
 		ChessMatch chessMatch = new ChessMatch();
-		Interface.printBoard(chessMatch.getPieces());
 		
-		//Testing chess moves inputs:
+		while (true) {
+			Interface.consoleClear();
+			Interface.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = Interface.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Destine: ");
+			ChessPosition destine = Interface.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, destine);
+		}
 		
-		System.out.println("Testing moviments:");
-		Interface.printBoard(chessMatch.getPieces());
-		System.out.print("\nSource position: ");
-		ChessPosition source = Interface.readChessPosition(sc);
-		System.out.print("Destine position: ");
-		ChessPosition destine = Interface.readChessPosition(sc);
-		ChessPiece capturedPiece = chessMatch.performChessMove(source, destine);
-		Interface.consoleClear();
-		Interface.printBoard(chessMatch.getPieces());
 		
-		sc.close();
 	}
 
 }

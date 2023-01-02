ee6bab5aff68a69db510c218f75f7739145da32d
antonio <asergiocosta.as@gmail.com>
2023-01-02 19:35:27 -0300

    Add some exceptions to the program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 //ALG - CHESS GAME APP
 // 30/12/2022
 package application;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chessGame.entities.ChessException;
 import chessGame.entities.ChessMatch;
 import chessGame.entities.ChessPiece;
 import chessGame.entities.ChessPosition;
@@ -15,17 +17,29 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			Interface.consoleClear();
-			Interface.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = Interface.readChessPosition(sc);
+			//CTRL+C inside the console to stop the app
+			try {
+				Interface.consoleClear();
+				Interface.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = Interface.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Destine: ");
+				ChessPosition destine = Interface.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, destine);
+			}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getLocalizedMessage());
+				sc.nextLine();
+			}
 			
-			System.out.println();
-			System.out.print("Destine: ");
-			ChessPosition destine = Interface.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, destine);
 		}
 		
 		
